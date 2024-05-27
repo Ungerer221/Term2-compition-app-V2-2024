710f45cc58ae46a2870cdff776090255e32cd681
Ungerer221 <221302@virtualwindow.co.za>
2024-05-27 18:19:14 +0200

    Plant Update Setup

--- farmlands-app/screens/plantDetailsScreen.jsx
+++ farmlands-app/screens/plantDetailsScreen.jsx
@@ -1,9 +1,15 @@
-import { View, Text, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Modal, Pressable, } from 'react-native'
 import React, { useState } from 'react'
 import { useFocusEffect } from '@react-navigation/native'
 import { getPlantItem } from '../services/plantDbService'
+//  Icons
+import { Feather } from '@expo/vector-icons';
+import MenuSquareIcon from '../icons/menuSquareIcon';
+import DashedLine02Icon from '../icons/dashed-line-02-stroke-rounded';
 
 export default function PlantDetailsScreen({ route, navigation }) {
+
+    const [modalVisible, setModalVisible] = useState(false);
 
     const [plantItem, setPlantItem] = useState([]) // creating a usestate
     const [itemName, setItemName] = useState()
@@ -36,10 +42,62 @@ export default function PlantDetailsScreen({ route, navigation }) {
     }
 
     return (
-        <View>
-            <Text>Name : {itemName}</Text>
-            <Text>Description Here: {itemDescription}</Text>
-            <Text>Growth Time: {itemGrowth}</Text>
+        <View style={styles.container}>
+            <View style={styles.textCon}>
+                <Text style={styles.textConText}>Name : </Text>
+                <View style={styles.textCon02}>
+                    <Text style={styles.textConText02}>{itemName}</Text>
+                </View>
+            </View>
+            <View style={styles.textCon}>
+                <Text style={styles.textConText}>Description Here: </Text>
+                <View style={styles.textCon02}>
+                    <Text style={styles.textConText02}>{itemDescription}</Text>
+                </View>
+            </View>
+            <View style={styles.textCon}>
+                <Text style={styles.textConText}>Growth Time: </Text>
+                <View style={styles.textCon02}>
+                    <Text style={styles.textConText02}>{itemGrowth}</Text>
+                </View>
+            </View>
+
+            {/* 
+            // TODO : Update Funciotnality 
+            // 1. Could use a modal with inputs to change the value 
+            // 2. could hide the input and when update is pressed it reveals the inputs and hides the data 
+            */}
+            <View>
+                {/* <Text style={styles.updateBtnText}>Update Item</Text> */}
+                <Modal
+                    animationType="slide"
+                    transparent={true}
+                    visible={modalVisible}
+                    onRequestClose={() => {
+                        Alert.alert('Modal has been closed.');
+                        setModalVisible(!modalVisible);
+                    }}>
+                    <View style={styles.centeredView}>
+                        {/* modal view */}
+                        <View style={styles.modalView}>
+                            <Text style={styles.modalText}>Update</Text>
+                            {/* close button */}
+                            <Pressable
+                                style={[styles.button, styles.buttonClose]}
+                                onPress={() => setModalVisible(!modalVisible)}>
+                                <Text style={styles.closeBtnText}>Hide Menu</Text>
+                            </Pressable>
+                        </View>
+                    </View>
+                </Modal>
+                <Pressable
+                    style={[styles.updateBtn, styles.buttonOpen]}
+                    onPress={() => setModalVisible(true)}>
+                    {/* <Text style={styles.textStyle}>Show Modal</Text> */}
+                    <MenuSquareIcon />
+                    <Text style={styles.updateBtnText}>Update Item</Text>
+                </Pressable>
+            </View>
         </View>
     )
 }
@@ -51,6 +109,82 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
         width: '100%',
         // backgroundColor:'gray',
+        padding: 20,
         gap: 10,
+    },
+    textCon: {
+        padding: 10,
+        width: '100%',
+        backgroundColor: '#eaeaea',
+        borderRadius: 12,
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    textConText: {
+        fontSize: 16,
+    },
+    textCon02: {
+        backgroundColor: '#F65774',
+        paddingVertical: 5,
+        paddingHorizontal: 15,
+        borderRadius: 12,
+    },
+    textConText02: {
+        color: '#fff',
+        fontSize: 16,
+        fontWeight: '700',
+    },
+    updateBtn: {
+        paddingHorizontal: 20,
+        paddingVertical: 15,
+        backgroundColor: '#A08BF6',
+        borderRadius: 12,
+        width: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    updateBtnText: {
+        color: '#fff',
+        fontWeight: '700',
+        fontSize: '20',
+    },
+    centeredView: {
+        // flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop:40,
+    },
+    modalView: {
+        marginTop: 40,
+        width: '95%',
+        height: '100%',
+        backgroundColor: 'white',
+        // borderRadius: 20,
+        borderTopLeftRadius: 22,
+        borderTopRightRadius:22,
+        padding: 35,
+        gap: 10,
+        alignItems: 'center',
+        justifyContent:'flex-start',
+        borderWidth: 2,
+
+        // shadowColor: '#000',
+        // shadowOffset: {
+        //     width: 0,
+        //     height: 2,
+        // },
+        // shadowOpacity: 0.25,
+        // shadowRadius: 4,
+        // elevation: 5,
+    },
+    buttonClose: {
+        padding: 20,
+        backgroundColor: '#F65774',
+        borderRadius: 24,
+    },
+    closeBtnText: {
+        fontSize: 16,
+        fontWeight: '700',
+        color: '#fff',
     },
 })
--- farmlands-app/services/plantDbService.js
+++ farmlands-app/services/plantDbService.js
@@ -1,27 +1,24 @@
 import { collection, addDoc, getDocs, query, orderBy, } from "firebase/firestore";
 import { db } from "../config/firebase";
-import { doc, getDoc } from "firebase/firestore";
+import { doc, getDoc, updateDoc } from "firebase/firestore";
 
 // * : create new list itm function 
 export const createNewPlantItem = async (item) => {
-
-  try {
-
-      // the addDoc it adding a new document and we need to specify where to add it - to a collection in our db - imported at the top -should pop up
-      // * to create a user -----------------------------------------------------------------------
-      // docref - our reference to our newly created document (brand ne with a self-generated ID)
-      const docRef = await addDoc(collection(db, "plants"), item);
-      console.log("Document written with ID: ", docRef.id);
-      // * : to navigate back to previous screen after filling in info
-      return true //  be more specific on why 
-      // we cant ascces the info yet
-      // console.log("document written with first: ", docRef.first);
-  } catch (e) {
-      console.error("Error adding document: ", e);
-      // * : to navigate back to previous screen after filling in info
-      return false
-  }
-
+    try {
+        // the addDoc it adding a new document and we need to specify where to add it - to a collection in our db - imported at the top -should pop up
+        // * to create a user -----------------------------------------------------------------------
+        // docref - our reference to our newly created document (brand ne with a self-generated ID)
+        const docRef = await addDoc(collection(db, "plants"), item);
+        console.log("Document written with ID: ", docRef.id);
+        // * : to navigate back to previous screen after filling in info
+        return true //  be more specific on why 
+        // we cant ascces the info yet
+        // console.log("document written with first: ", docRef.first);
+    } catch (e) {
+        console.error("Error adding document: ", e);
+        // * : to navigate back to previous screen after filling in info
+        return false
+    }
 }
 
 
@@ -39,7 +36,7 @@ export const getAllPlantsList = async () => {
     return allPlants
 }
 
-// TODO : Get single Item 
+// * : Get single Item 
 // possable method
 // 1. get id of the item you pressed
 // 2. have that item id = var
@@ -55,4 +52,16 @@ export const getPlantItem = async (itemID) => {
         // docSnap.data() will be undefined in this case
         console.log("No such document!");
     }
+}
+
+// TODO : Update Functionality
+export const updatePlantItem = async (itemID) => {
+    const docRef = doc(db, "plants", itemID);
+    await updateDoc(docRef, {
+        name: '', // this is what we are changing 
+        description: '',
+        growthTime: '',
+    });
+    //   const docRef = await updateDoc(collection(db, "plants"), item);
+    //   console.log("Document written with ID: ", docRef.id);
 }
